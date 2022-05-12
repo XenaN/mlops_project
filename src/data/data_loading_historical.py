b5c2e23b3a77a5d2b1f3cf39740f344cb6114d0d
Xena <43779450+XenaN@users.noreply.github.com>
2022-05-12 09:02:39 +0700

    formatted code

--- src/data/data_loading_historical.py
+++ src/data/data_loading_historical.py
@@ -10,14 +10,15 @@ import pandas as pd
 
 
 SERVICE_URL = "http://discomap.eea.europa.eu/map/fme/latest"
-HISTORICAL_EEA_PATH = 'data/raw/historical_data_eea/'
-UPDATED_EEA_PATH = '../../data/raw/updated_data_eea/'
-TAGS_PATH = 'metadata/download_tags.json'
-METADATA_PATH = 'metadata/download_config.json'
+HISTORICAL_EEA_PATH = "data/raw/historical_data_eea/"
+UPDATED_EEA_PATH = "../../data/raw/updated_data_eea/"
+TAGS_PATH = "metadata/download_tags.json"
+METADATA_PATH = "metadata/download_config.json"
 
 
-def download_urls_historical_data_from_discomap(country: str, pollutant: str,
-                                                year_start: int, year_end: int):
+def download_urls_historical_data_from_discomap(
+    country: str, pollutant: str, year_start: int, year_end: int
+):
     """
     Save urls with historical data from discomap.eea.europa.eu
     :param country: tag of country
@@ -28,13 +29,17 @@ def download_urls_historical_data_from_discomap(country: str, pollutant: str,
     with open(TAGS_PATH) as json_file:
         metadata = json.load(json_file)
 
-    with open(f'{HISTORICAL_EEA_PATH}{country}_{pollutant}_urls.txt', 'wb') as urls_file:
+    with open(
+        f"{HISTORICAL_EEA_PATH}{country}_{pollutant}_urls.txt", "wb"
+    ) as urls_file:
 
-        download_file = f"https://fme.discomap.eea.europa.eu/fmedatastreaming/" \
-                        f"AirQualityDownload/AQData_Extract.fmw?CountryCode={country}&City" \
-                        f"Name=&Pollutant={metadata[pollutant]}&Year_from={year_start}" \
-                        f"&Year_to={year_end}&Station=&Samplingpoint=&" \
-                        f"Source=All&Output=TEXT&UpdateDate=&TimeCoverage=Year"
+        download_file = (
+            f"https://fme.discomap.eea.europa.eu/fmedatastreaming/"
+            f"AirQualityDownload/AQData_Extract.fmw?CountryCode={country}&City"
+            f"Name=&Pollutant={metadata[pollutant]}&Year_from={year_start}"
+            f"&Year_to={year_end}&Station=&Samplingpoint=&"
+            f"Source=All&Output=TEXT&UpdateDate=&TimeCoverage=Year"
+        )
         try:
             status = requests.get(download_file).status_code
             if status < 300:
@@ -53,14 +58,17 @@ def save_csv_from_url(pair_path_and_url: Tuple):
     """
     try:
         dataset = pd.read_csv(pair_path_and_url[1], index_col=False)
-        dataset.to_csv(f"{pair_path_and_url[0]}{pair_path_and_url[1].split('/')[-1]}", index=False)
+        dataset.to_csv(
+            f"{pair_path_and_url[0]}{pair_path_and_url[1].split('/')[-1]}",
+            index=False
+        )
     except Exception:
         print(f"Save csv file fail {pair_path_and_url[1]}")
 
 
 @click.command()
-@click.argument('pollutant', type=click.STRING)
-@click.argument('n_cores', type=click.INT)
+@click.argument("pollutant", type=click.STRING)
+@click.argument("n_cores", type=click.INT)
 def download_historical_data_from_discomap_urls(pollutant: str, n_cores: int):
     """
     Save historical data from discomap.eea.europa.eu urls
@@ -70,13 +78,22 @@ def download_historical_data_from_discomap_urls(pollutant: str, n_cores: int):
     with open(METADATA_PATH) as json_file:
         metadata = json.load(json_file)
 
-    download_urls_historical_data_from_discomap(metadata["country"], pollutant,
-                                                metadata["year_start"], metadata["year_end"])
+    download_urls_historical_data_from_discomap(
+        metadata["country"],
+        pollutant,
+        metadata["year_start"],
+        metadata["year_end"]
+    )
 
-    with open(f'{HISTORICAL_EEA_PATH}{metadata["country"]}_{pollutant}_urls.txt', 'r', encoding='utf-8-sig') as file:
+    with open(
+        f'{HISTORICAL_EEA_PATH}{metadata["country"]}_{pollutant}_urls.txt',
+        "r",
+        encoding="utf-8-sig",
+    ) as file:
         urls = file.read().splitlines()
 
-    country_pollutant_path = f"{HISTORICAL_EEA_PATH}/{metadata['country']}_{pollutant}/"
+    country_pollutant_path = f"{HISTORICAL_EEA_PATH}/{metadata['country']}_" \
+                             f"{pollutant}/"
     pathlib.Path(country_pollutant_path).mkdir(parents=True, exist_ok=True)
 
     with Pool(processes=n_cores) as pool:
@@ -88,4 +105,3 @@ def download_historical_data_from_discomap_urls(pollutant: str, n_cores: int):
 if __name__ == "__main__":
     pathlib.Path(HISTORICAL_EEA_PATH).mkdir(parents=True, exist_ok=True)
     download_historical_data_from_discomap_urls()
-
--- src/data/data_raw_merge.py
+++ src/data/data_raw_merge.py
@@ -7,28 +7,30 @@ from datetime import date
 import pandas as pd
 
 
-INTERIM_UPDATED_EEA_PATH = 'data/interim/updated_data_eea/'
-RAW_HISTORICAL_EEA_PATH = 'data/raw/historical_data_eea/'
-RAW_UPDATED_EEA_PATH = 'data/raw/updated_data_eea/'
-METADATA_PATH = 'metadata/download_tags.json'
-CONFIG_PATH = 'metadata/download_config.json'
+INTERIM_UPDATED_EEA_PATH = "data/interim/updated_data_eea/"
+RAW_HISTORICAL_EEA_PATH = "data/raw/historical_data_eea/"
+RAW_UPDATED_EEA_PATH = "data/raw/updated_data_eea/"
+METADATA_PATH = "metadata/download_tags.json"
+CONFIG_PATH = "metadata/download_config.json"
 
-COLUMNS = {'network_countrycode': 'Countrycode',
-           'network_localid': 'AirQualityNetwork',
-           'network_namespace': 'Namespace',
-           'pollutant': 'AirPollutant',
-           'samplingpoint_localid': 'SamplingPoint',
-           'station_code': 'AirQualityStationEoICode',
-           'value_unit': 'UnitOfMeasurement',
-           'value_verification': 'Verification',
-           'value_validity': 'Validity',
-           'value_datetime_begin': 'DatetimeBegin',
-           'value_datetime_end': 'DatetimeEnd',
-           'value_numeric': 'Concentration'}
+COLUMNS = {
+    "network_countrycode": "Countrycode",
+    "network_localid": "AirQualityNetwork",
+    "network_namespace": "Namespace",
+    "pollutant": "AirPollutant",
+    "samplingpoint_localid": "SamplingPoint",
+    "station_code": "AirQualityStationEoICode",
+    "value_unit": "UnitOfMeasurement",
+    "value_verification": "Verification",
+    "value_validity": "Validity",
+    "value_datetime_begin": "DatetimeBegin",
+    "value_datetime_end": "DatetimeEnd",
+    "value_numeric": "Concentration",
+}
 
 
 @click.command()
-@click.argument('pollutant', type=click.STRING)
+@click.argument("pollutant", type=click.STRING)
 def merge_eea_data(pollutant: str):
     """
     This function concatenates historical and last updated datasets
@@ -41,7 +43,8 @@ def merge_eea_data(pollutant: str):
     with open(CONFIG_PATH) as json_file:
         config = json.load(json_file)
 
-    file_name = f"{RAW_UPDATED_EEA_PATH}{config['country']}_{metadata[pollutant]}_{date_today}.csv"
+    file_name = f"{RAW_UPDATED_EEA_PATH}{config['country']}_" \
+                f"{metadata[pollutant]}_{date_today}.csv"
 
     if len(os.listdir(RAW_UPDATED_EEA_PATH)) == 0:
         print(f"{file_name} does not exist")
@@ -56,31 +59,48 @@ def merge_eea_data(pollutant: str):
     list_csv = os.listdir(path)
     updated_data = pd.read_csv(file_name,
                                index_col=False,
-                               encoding='latin1').rename(COLUMNS, axis='columns')
+                               encoding="latin1").rename(
+        COLUMNS, axis="columns"
+    )
 
     for file in list_csv:
-        historical_data = pd.read_csv(f"{path}{file}", index_col=False, encoding='latin1')
+        historical_data = pd.read_csv(
+            f"{path}{file}", index_col=False, encoding="latin1"
+        )
 
-        code = config['AirQualityStationEoICode']
-        assert len(historical_data['AirQualityStationEoICode'].unique()) == 1
+        code = config["AirQualityStationEoICode"]
+        assert len(historical_data["AirQualityStationEoICode"].unique()) == 1
 
-        if historical_data['AirQualityStationEoICode'].unique()[0] == code:
+        if historical_data["AirQualityStationEoICode"].unique()[0] == code:
 
-            data_new = pd.concat([
-                historical_data,
-                updated_data.query("AirQualityStationEoICode == @code").sort_values("DatetimeEnd")
-            ], axis=0, join="inner")
+            data_new = pd.concat(
+                [
+                    historical_data,
+                    updated_data.query(
+                        "AirQualityStationEoICode == @code").sort_values(
+                        "DatetimeEnd"
+                    )
+                ],
+                axis=0,
+                join="inner",
+            )
 
-            data_new['DatetimeBegin'] = data_new['DatetimeBegin'].apply(lambda x: x.replace(" +", "+"))
-            data_new['DatetimeEnd'] = data_new['DatetimeEnd'].apply(lambda x: x.replace(" +", "+"))
+            data_new["DatetimeBegin"] = data_new["DatetimeBegin"].apply(
+                lambda x: x.replace(" +", "+")
+            )
+            data_new["DatetimeEnd"] = data_new["DatetimeEnd"].apply(
+                lambda x: x.replace(" +", "+")
+            )
             data_new = data_new.drop_duplicates()
 
-            data_new.to_csv(f"{INTERIM_UPDATED_EEA_PATH}{config['country']}_{pollutant}_historical_updated.csv", index=False)
+            data_new.to_csv(
+                f"{INTERIM_UPDATED_EEA_PATH}{config['country']}_"
+                f"{pollutant}_historical_updated.csv",
+                index=False,
+            )
 
 
 if __name__ == "__main__":
     pathlib.Path(INTERIM_UPDATED_EEA_PATH).mkdir(parents=True, exist_ok=True)
 
     merge_eea_data()
-
-
