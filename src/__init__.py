f21b2bdc375ece1d3c84372e3ad0f9c595f24b92
Xena <43779450+XenaN@users.noreply.github.com>
2022-05-31 16:09:27 +0700

    formatted init.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,12 @@
 from .data.clean_data import clean_data
 from .data.filter_data import filter_data
-from .data.data_loading_historical import download_historical_data_from_discomap_urls, \
-    save_csv_from_url, download_urls_historical_data_from_discomap
+from .data.data_loading_historical import (
+    download_historical_data_from_discomap_urls,
+    save_csv_from_url,
+    download_urls_historical_data_from_discomap,
+)
 from .data.data_loading_updated import download_updated_data_from_discomap
 from .data.data_raw_merge import merge_eea_data
 from .features.data_preprocessing import create_common_dataset, change_units
 from .models.train import train
 from .models.prepare_datasets import prepare_dataset
-
-
