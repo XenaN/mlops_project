f36e0daf4b325efa7759ef5f09dcd0e479cad0a0
Xena <43779450+XenaN@users.noreply.github.com>
2022-06-01 15:32:18 +0700

    remover testclient

--- tests/test_post_request.py
+++ tests/test_post_request.py
@@ -3,19 +3,14 @@ from src.app.inference import app
 import requests
 
 TEST_FILE = "data/test_x.csv"
-client = TestClient(app)
 
 
 def test_create_request():
     files = {"file": open(TEST_FILE, "rb")}
-    response = client.post(
-        "/invocation",
-        files=files,
-    )
-
     url = "http://192.168.0.157:8003/invocation"
     resp = requests.post(url=url, files=files)
+    result = resp.json()
 
-    assert response.status_code == 200
     assert resp.status_code == 200
-    assert response.json() == resp.json()
+    assert type(result) is list
+    assert type(result[0]) is float
